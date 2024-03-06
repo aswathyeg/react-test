c24c9bfb338030e6701b42457d96e624adc6bba2
Aswathy E G <aswathy.eg.bijesh@gmail.com>
2024-03-06 14:48:30 +0530

    used context in one component

--- src/App.js
+++ src/App.js
@@ -1,31 +1,27 @@
-import Section from './components/context/withoutContext/Section.js';
+import Section from './components/context/withContext/Section.js';
 
-import Heading from './components/context/withoutContext/Heading.js';
+import Heading from './components/context/withContext/Heading.js';
 
 export default function App() {
-  //Let’s say you want multiple headings within the same Section to always have the same size:
-  //Currently, you pass the level prop to each <Heading> separately:
-  //It would be nice if you could pass the level prop to the <Section> component instead and remove it from the <Heading>.
-  // This way you could enforce that all headings in the same section have the same size:
-
-//But how can the <Heading> component know the level of its closest <Section>? 
-//That would require some way for a child to “ask” for data from somewhere above in the tree.
-//solution:context
+  
   return (
-    <Section>
-      <Heading level={1}>Title</Heading>
-      <Section>
-        <Heading level={2}>Heading</Heading>
-        <Heading level={2}>Heading</Heading>
-        <Heading level={2}>Heading</Heading>
-        <Section>
-          <Heading level={3}>Sub-heading</Heading>
-          <Heading level={3}>Sub-heading</Heading>
-          <Heading level={3}>Sub-heading</Heading>
-          <Section>
-            <Heading level={4}>Sub-sub-heading</Heading>
-            <Heading level={4}>Sub-sub-heading</Heading>
-            <Heading level={4}>Sub-sub-heading</Heading>
+    <Section >
+      <Heading >Title</Heading>
+      <Section >
+        <Heading >Heading</Heading>
+        <Heading >Heading</Heading>
+        <Heading>Heading</Heading>
+
+        <Section >
+          <Heading >Sub-heading</Heading>
+          <Heading >Sub-heading</Heading>
+          <Heading >Sub-heading</Heading>
+
+          <Section >
+            <Heading >Sub-sub-heading</Heading>
+            <Heading >Sub-sub-heading</Heading>
+            <Heading >Sub-sub-heading</Heading>
+
           </Section>
         </Section>
       </Section>
