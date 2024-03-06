844c0557068e995e93a37c7cb515e584d685523b
Aswathy E G <aswathy.eg.bijesh@gmail.com>
2024-03-06 14:49:45 +0530

    comments

--- src/components/context/withoutContext/Heading.js
+++ src/components/context/withoutContext/Heading.js
@@ -16,4 +16,13 @@ export default function Heading({ level, children }) {
         throw Error('Unknown level: ' + level);
     }
   }
-  
+  //notes to explain propdrilling without context
+  //Let’s say you want multiple headings within the same Section to always have the same size:
+  //Currently, you pass the level prop to each <Heading> separately:
+  //It would be nice if you could pass the level prop to the <Section> component instead and remove it from the <Heading>.
+  // This way you could enforce that all headings in the same section have the same size:
+
+//But how can the <Heading> component know the level of its closest <Section>? 
+//That would require some way for a child to “ask” for data from somewhere above in the tree.
+//solution:context
+//Context lets a parent—even a distant one!—provide some data to the entire tree inside of it.
