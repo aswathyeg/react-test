d1d75c617b59b4a4823511aeb1616af15fe8531f
Aswathy E G <aswathy.eg.bijesh@gmail.com>
2024-03-08 23:35:02 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,30 +1,11 @@
-import Section from './components/context/withContext/Section.js';
 
-import Heading from './components/context/withContext/Heading.js';
+import RefExample from './components/refs/RefExample.js';
 
 export default function App() {
   
   return (
-    <Section >
-      <Heading >Title</Heading>
-      <Section >
-        <Heading >Heading</Heading>
-        <Heading >Heading</Heading>
-        <Heading>Heading</Heading>
-
-        <Section >
-          <Heading >Sub-heading</Heading>
-          <Heading >Sub-heading</Heading>
-          <Heading >Sub-heading</Heading>
-
-          <Section >
-            <Heading >Sub-sub-heading</Heading>
-            <Heading >Sub-sub-heading</Heading>
-            <Heading >Sub-sub-heading</Heading>
-
-          </Section>
-        </Section>
-      </Section>
-    </Section>
+   <div>
+<RefExample/>
+   </div>
   );
 }
