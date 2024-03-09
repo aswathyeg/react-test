7ad6d1c89a4ddc55f928154875058461fffbc3a4
Aswathy E G <aswathy.eg.bijesh@gmail.com>
2024-03-09 16:58:14 +0530

    added ref

--- src/components/refs/RefExample.js
+++ src/components/refs/RefExample.js
@@ -1,17 +1,38 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
 
 const RefExample = () => {
-  const ref=useRef(0);
+  const [starTime,setStartTime]=useState(null)
+  const[now,setNow]=useState(null)
+
+  const ref=useRef(null);
+
+const handleStart=()=>{
+  setStartTime(Date.now())
+setNow(Date.now())
+clearInterval(ref.current);
+ref.current=setInterval(() => {
+  setNow(Date.now())
+  
+}, 10);
 
 
+}
+const handleStop=()=>{
+  clearInterval(ref.current)
+
+}
+let secondsPassed=0;
+if(starTime!==0&&now!==0){
+  secondsPassed=(now-starTime)/1000;
+}
 
   return (
     <div>
       <h1>
-        Time passed:{}
+        Time passed:{secondsPassed.toFixed(3)}
       </h1>
-      <button>start</button>
-      <button>stop</button>
+      <button onClick={handleStart}>start</button>
+      <button onClick={handleStop}>stop</button>
       </div>
   )
 }
