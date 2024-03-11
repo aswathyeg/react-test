0105f6647c883fadb94c3b37a92aac7dc997edca
Aswathy E G <aswathy.eg.bijesh@gmail.com>
2024-03-11 22:39:34 +0530

    upddated

--- src/components/refs/RefExample.js
+++ src/components/refs/RefExample.js
@@ -1,40 +1,35 @@
-import React, { useRef, useState } from 'react'
+import React, { useRef, useState } from "react";
 
 const RefExample = () => {
-  const [starTime,setStartTime]=useState(null)
-  const[now,setNow]=useState(null)
+  const [starTime, setStartTime] = useState(null);
+  const [now, setNow] = useState(null);
 
-  const ref=useRef(null);
+  const ref = useRef(null);
 
-const handleStart=()=>{
-  setStartTime(Date.now())
-setNow(Date.now())
-clearInterval(ref.current);
-ref.current=setInterval(() => {
-  setNow(Date.now())
-  
-}, 10);
+  const handleStart = () => {
+    setStartTime(Date.now());
+    setNow(Date.now());
+    clearInterval(ref.current);
 
-
-}
-const handleStop=()=>{
-  clearInterval(ref.current)
-
-}
-let secondsPassed=0;
-if(starTime!==0&&now!==0){
-  secondsPassed=(now-starTime)/1000;
-}
+    ref.current = setInterval(() => {
+      setNow(Date.now());
+    }, 10);
+  };
+  const handleStop = () => {
+    clearInterval(ref.current);
+  };
+  let secondsPassed = 0;
+  if (starTime !== 0 && now !== 0) {
+    secondsPassed = (now - starTime) / 1000;
+  }
 
   return (
     <div>
-      <h1>
-        Time passed:{secondsPassed.toFixed(3)}
-      </h1>
+      <h1>Time passed:{secondsPassed.toFixed(3)}</h1>
       <button onClick={handleStart}>start</button>
       <button onClick={handleStop}>stop</button>
-      </div>
-  )
-}
+    </div>
+  );
+};
 
 export default RefExample;
